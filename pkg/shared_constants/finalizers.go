1dfd273f43e308388c87b58fe6cb854976bae1c5
kaposmee <kaposmee@amazon.com>
2026-08-18 19:38:24 +0000

    feat(gateway custom): allow importers to customize gateway config finalizers

--- pkg/shared_constants/finalizers.go
+++ pkg/shared_constants/finalizers.go
@@ -10,14 +10,22 @@ const (
 	// ServiceFinalizer the finalizer used on service resources
 	ServiceFinalizer = "service.k8s.aws/resources"
 
-	// GatewayClassFinalizer the finalizer we attach to an in-use LBC GatewayClass
-	GatewayClassFinalizer = "gateway.k8s.aws/gatewayclass"
-
 	// NLBGatewayFinalizer the finalizer we attach to an NLB Gateway resource
 	NLBGatewayFinalizer = "gateway.k8s.aws/nlb"
 
 	// ALBGatewayFinalizer the finalizer we attach to an ALB Gateway resource
 	ALBGatewayFinalizer = "gateway.k8s.aws/alb"
+
+	// GlobalAcceleratorFinalizer the finalizer we attach to a global accelerator resource
+	GlobalAcceleratorFinalizer = "aga.k8s.aws/resources"
+)
+
+// Finalizers on GatewayClass, LoadBalancerConfiguration, TargetGroupConfiguration, and
+// ListenerRuleConfiguration are var so importers embedding this controller can rebrand them at
+// init time (mirroring the pkg/gateway/constants controllerName vars introduced in #4878).
+var (
+	// GatewayClassFinalizer the finalizer we attach to an in-use LBC GatewayClass
+	GatewayClassFinalizer = "gateway.k8s.aws/gatewayclass"
 
 	// TargetGroupConfigurationFinalizer the finalizer we attach to a target group configuration resource
 	TargetGroupConfigurationFinalizer = "gateway.k8s.aws/targetgroupconfigurations"
@@ -27,7 +35,4 @@ const (
 
 	// ListenerRuleConfigurationFinalizer the finalizer we attach to a listener rule configuration resource
 	ListenerRuleConfigurationFinalizer = "gateway.k8s.aws/listenerruleconfigurations"
-
-	// GlobalAcceleratorFinalizer the finalizer we attach to a global accelerator resource
-	GlobalAcceleratorFinalizer = "aga.k8s.aws/resources"
 )
